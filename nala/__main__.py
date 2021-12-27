c1dd935c51d3509897188a9644b7a7473a768d07
Blake Lee <blake@volian.org>
2021-12-27 17:32:28 -0500

    refactor(main): increase code quality
    refactor(main): increase code quality
    
    code refactored based on suggestions from Sourcery-ai that could not be automated

--- nala/__main__.py
+++ nala/__main__.py
@@ -62,104 +62,118 @@ def _main():
 		exit(f"Nala needs root to {command}")
 
 	if command in apt_init:
-		no_update_list = ('remove', 'show', 'history', 'install', 'purge')
-		if command in no_update_list:
-			no_update = True
-		if update:
-			no_update = False
-
-		apt = nala(
-			download_only=download_only,
-			assume_yes=assume_yes,
-			no_update=no_update,
-			debug=debug,
-			verbose=verbose,
-			raw_dpkg=raw_dpkg
+		apt = init_apt(
+			command, update, 
+			download_only, assume_yes,
+			no_update, debug,
+			verbose, raw_dpkg
 		)
 
 	if command in ('update', 'upgrade'):
 		apt.upgrade(dist_upgrade=arguments.no_full)
 
-	if command == 'install':
-		args = arguments.args
-		if not args:
+	elif command == 'install':
+		if not arguments.args:
 			print('You must specify a package to install')
 			exit()
-		apt.install(args)
+		apt.install(arguments.args)
 
-	if command in ('remove', 'purge'):
+	elif command in ('remove', 'purge'):
 		purge = command == 'purge'
 		args = arguments.args
 		apt.remove(args, purge=purge)
 
-	if command == 'fetch':
-		foss = arguments.foss
-		fetches = arguments.fetches
-		debian = arguments.debian
-		ubuntu = arguments.ubuntu
-		country = arguments.country
+	elif command == 'fetch':
+		fetch(
+			arguments.fetches, arguments.foss,
+			arguments.debian, arguments.ubuntu,
+			arguments.country, assume_yes
+		)
 
-		fetch(fetches, foss, debian, ubuntu, country, assume_yes)
-
-	if command == 'show':
-		args = arguments.args
-		if not args:
+	elif command == 'show':
+		if not arguments.args:
 			print('You must specify a package to show')
 			exit()
-		apt.show(args)
+		apt.show(arguments.args)
 
-	if command == 'history':
-		id = arguments.id
-		mode = arguments.mode
+	elif command == 'history':
+		history(arguments, apt, su)
 
-		if mode:
-			if not id:
-				print('We need a transaction ID..')
-				exit()
-		else:
-			apt.history()
-
-		if mode in ('undo', 'redo', 'info'):
-			try:
-				id = int(id)
-			except ValueError:
-				print('Option must be a number..')
-				exit()
-
-		if mode == 'undo':
-			apt.history_undo(id)
-
-		elif mode == 'redo':
-			apt.history_undo(id, redo=True)
-
-		elif mode == 'info':
-				apt.history_info(id)
-
-		elif mode == 'clear':
-			if su != 0:
-				esyslog(f'{getuser()} tried to run [{" ".join(argv)}] without permission')
-				exit('Nala needs root to clear history')
-			apt.history_clear(id)
-
-	if command == 'clean':
+	elif command == 'clean':
 		shell.apt.clean()
 		print("Nala's local cache has been cleaned up")
 
-	if command == 'moo':
-		moos = arguments.moo
-		moos = moos.count('moo')
-		dprint(f"moo number is {moos}")
-		if moos == 1:
-			print(LION_ASCII)
-		elif moos == 2:
-			print(LION_ASCII2)
-		else:
-			print(CAT_ASCII)
-		print('..."I can\'t moo for I\'m a cat"...')
-		if no_update:
-			print("...What did you expect no-update to do?...")
-		if update:
-			print("...What did you expect to update?...")
+	elif command == 'moo':
+		moo_pls(arguments, update, no_update)
+
+def history(arguments, apt: nala, su):
+	id = arguments.id
+	mode = arguments.mode
+
+	if mode and not id:
+		print('We need a transaction ID..')
+		exit()
+	else:
+		apt.history()
+
+	if mode in ('undo', 'redo', 'info'):
+		try:
+			id = int(id)
+		except ValueError:
+			print('Option must be a number..')
+			exit()
+
+	if mode == 'undo':
+		apt.history_undo(id)
+
+	elif mode == 'redo':
+		apt.history_undo(id, redo=True)
+
+	elif mode == 'info':
+		apt.history_info(id)
+
+	elif mode == 'clear':
+		if su != 0:
+			esyslog(f'{getuser()} tried to run [{" ".join(argv)}] without permission')
+			exit('Nala needs root to clear history')
+		apt.history_clear(id)
+
+def init_apt(
+	command, update, 
+	download_only, assume_yes,
+	no_update, debug,
+	verbose, raw_dpkg):
+
+	no_update_list = ('remove', 'show', 'history', 'install', 'purge')
+	if command in no_update_list:
+		no_update = True
+	if update:
+		no_update = False
+
+	return nala(
+		download_only=download_only,
+		assume_yes=assume_yes,
+		no_update=no_update,
+		debug=debug,
+		verbose=verbose,
+		raw_dpkg=raw_dpkg
+	)
+
+def moo_pls(arguments, update, no_update):
+	moos = arguments.moo
+	moos = moos.count('moo')
+	dprint(f"moo number is {moos}")
+	if moos == 1:
+		print(LION_ASCII)
+	elif moos == 2:
+		print(LION_ASCII2)
+	else:
+		print(CAT_ASCII)
+	print('..."I can\'t moo for I\'m a cat"...')
+	if no_update:
+		print("...What did you expect no-update to do?...")
+	if update:
+		print("...What did you expect to update?...")
 
 def main():
 	try:
