82a19b141ad5d874d5198c6ad22354ad63a5d9ac
Blake Lee <blake@volian.org>
2022-01-02 02:07:05 +0000

    docs(license): update/add copyright, license notices
    docs(license): add missing copyright, license notices
    docs(license): update copyright, license notices
    docs(license): add original apt-metalink copyright notice
    
    We are a fork of apt-metalink. I must retain their copyright notice. Crediting the author and maintaining the GPLv3-or-later is not enough

--- nala-cli.py
+++ nala-cli.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+#                 __
+#    ____ _____  |  | _____
+#   /    \\__  \ |  | \__  \
+#  |   |  \/ __ \|  |__/ __ \_
+#  |___|  (____  /____(____  /
+#       \/     \/          \/
+#
+# Copyright (C) 2021, 2022 Blake Lee
+#
+# This file is part of nala
+#
+# nala is program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# nala is program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with nala.  If not, see <https://www.gnu.org/licenses/>.
 
 """Nala command-line entry point"""
 
--- nala/__main__.py
+++ nala/__main__.py
@@ -1,23 +1,26 @@
-# nala is a wrapper for the apt package manager.
-# Copyright (C) 2021 Volitank
-
-# nala is free software: you can redistribute it and/or modify
+#                 __
+#    ____ _____  |  | _____
+#   /    \\__  \ |  | \__  \
+#  |   |  \/ __ \|  |__/ __ \_
+#  |___|  (____  /____(____  /
+#       \/     \/          \/
+#
+# Copyright (C) 2021, 2022 Blake Lee
+#
+# This file is part of nala
+#
+# nala is program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
-# nala is distributed in the hope that it will be useful,
+#
+# nala is program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.tures
-
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
 # along with nala.  If not, see <https://www.gnu.org/licenses/>.
-
-## Special thanks to Max Taggart for Columnar. nala uses a modified version
-## https://github.com/MaxTaggart/columnar
-## Special thanks to Tatsuhiro Tsujikawa for apt-metalink. nala uses a modified version
-## https://github.com/tatsuhiro-t/apt-metalink
 
 import logging
 from getpass import getuser
