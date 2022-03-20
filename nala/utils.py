9a8ede6af7be101bdb8115da7973bcca2da8212e
Blake Lee <blake@volian.org>
2022-03-19 23:35:47 -0400

    refactor: change `--fix-broken` to use the cache function.

--- nala/utils.py
+++ nala/utils.py
@@ -469,6 +469,8 @@ def dedupe_list(original: list[str]) -> list[str]:
 
 def get_summary_header(history: bool = False) -> tuple[str, str, str, str]:
 	"""Return the correct headers for the summary."""
+	if not arguments.command and arguments.fix_broken:
+		return _('Remove'), _('Removing:'), _('Auto-Remove'), _('Auto-Removing:')
 	if 'purge' in arguments.command and history:
 		return _('Purged'), _('Purged:'), _('Auto-Purged'), _('Auto-Purged:')
 	if 'purge' in arguments.command and not history:
